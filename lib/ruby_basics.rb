05de72664c270875ec9180d659bb7cfe2005b635
reflective-mapping-9478 <ejrobe@gmail.com>
2018-11-13 09:09:43 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
 
 def assign_variable(value)
-  
+  return value
 end
 
-def argue
-  
+def argue(x)
+  x
 end
 
-def greeting
-
+def greeting(x, y)
+ x+y
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
